90633ddef3e2cdb1a77ac847fd2666b231324953
Alex Rozumey <brain-geek@yandex.ua>
2011-05-05 15:59:45 +0300

    Clean up discover.rb, gitignore

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,10 +1,1 @@
-require 'autotest/restart' # optional: forces autotest to pick the changes to this file
-require 'autotest/timestamp' # optional: shows timestamps for test runs
-
 Autotest.add_discovery { "rspec2" }
-
-Autotest.add_hook :initialize do |at|
-  %w{.git .svn .hg .DS_Store ._*}.each do |exception|
-    at.add_exception(exception)
-  end
-end
